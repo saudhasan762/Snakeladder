9f62049d2c2087c2ed44cc73ba43f2dbe83dec8a
saudhasan762 <azanali152@gmail.com>
2021-02-13 09:24:01 +0530

    Modified naming conventions and Indentation

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,76 +1,72 @@
 
 public class SnakeLadder {
 	//constants
-	public static final int start_position=0;
-	public static final int end_position=100;
-	public static final int no_play_option=0;
-	public static final int ladder_option=1;
-	public static final int snake_option=2;
+	public static final int startPosition=0;
+	public static final int endPosition=100;
+	public static final int noPlay=0;
+	public static final int ladder=1;
+	public static final int snake=2;
 	
-	public static int count_dice=0;
-	public static int player_position=0;
-	public int previous_position=0;
-	public int updated_position=0;
-	public static int player_one_position=0;
-	public static int player_two_position=0;
-	public static int select_option;
+	public static int diceCount=0;
+	public static int playerPosition=0;
+	public int previousPosition=0;
+	public int updatedPosition=0;
+	public static int playerOnePosition=0;
+	public static int playerTwoPosition=0;
+	public static int selectOption;
 	
 	public static void main(String[] args) {
 		SnakeLadder Player1 = new SnakeLadder();
 		SnakeLadder Player2 = new SnakeLadder();
-		while( player_position >= start_position && player_position <= end_position){
+		while( playerPosition >= startPosition && playerPosition <= endPosition){
 			Player1.rolldice();
-			if (select_option == ladder_option)
+			if (selectOption == ladder)
 				Player1.rolldice();
-			player_one_position=player_position;
-			System.out.println("Player one position "+player_one_position);
+			playerOnePosition = playerPosition;
+			System.out.println("Player one position "+playerOnePosition);
 			
-			if(player_one_position == end_position) {	
+			if(playerOnePosition == endPosition) {	
 				System.out.println("Player One Wins");
-				break;
-			}	
+				break; }
 			
 			Player2.rolldice();
-			if (select_option == ladder_option)
+			if (selectOption == ladder)
 				Player2.rolldice();
-			player_two_position=player_position;
-			System.out.println("Player two position "+player_one_position);
-			if(player_two_position == end_position) {		
+			playerTwoPosition = playerPosition;
+			System.out.println("Player two position "+playerOnePosition);
+			if(playerTwoPosition == endPosition) {		
 				System.out.println("Player Two Wins");
-				break;
-			}					
+				break; }					
 		} 
-		System.out.println("Number of dice roll "+count_dice);				
+		System.out.println("Number of dice roll "+diceCount);				
 	}
 	
 	public int rolldice() {
-		count_dice = count_dice + 1;
-		int value=(int)Math.floor(Math.random()*10 % 6);
-		value =value + 1;
-		select_option=value % 3;
+		diceCount = diceCount + 1;
+		int diceValue = (int)Math.floor(Math.random()*10 % 6);
+		diceValue = diceValue + 1;
+		selectOption = diceValue % 3;
 		
-		if( select_option == no_play_option )
-			player_position = previous_position;
+		if( selectOption == noPlay )
+			playerPosition = previousPosition;
 		
-		else if(select_option == ladder_option) {
-			updated_position = previous_position + value;
-			if( updated_position > end_position )
-				player_position=previous_position;
-			else if( updated_position == end_position)
-				player_position = end_position;
+		else if(selectOption == ladder) {
+			updatedPosition = previousPosition + diceValue;
+			if( updatedPosition > endPosition )
+				playerPosition = previousPosition;
+			else if( updatedPosition == endPosition)
+				playerPosition = endPosition;
 			else
-				player_position = updated_position;
-			previous_position=player_position;
-		}
+				playerPosition = updatedPosition;
+			previousPosition = playerPosition;	}
 		
 		else {
-			updated_position = previous_position - value;
-			if(updated_position < start_position)
-				player_position = start_position;
+			updatedPosition = previousPosition - diceValue;
+			if(updatedPosition < startPosition)
+				playerPosition = startPosition;
 			else
-				player_position = updated_position;
-			previous_position = player_position;
-		}
-		return count_dice;		
+				playerPosition = updatedPosition;
+			previousPosition = playerPosition;	}
+		return diceCount;		
 	}
 }
